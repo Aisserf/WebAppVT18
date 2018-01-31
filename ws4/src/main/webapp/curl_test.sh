efff0fe1f65df167de227736289e49a8ba20b618
Aisserf <Aisserf@users.noreply.github.com>
2018-01-31 10:28:31 +0100

    uppdaterad curl_test.sh

--- ws4/src/main/webapp/curl_test.sh
+++ ws4/src/main/webapp/curl_test.sh
@@ -8,27 +8,27 @@
 # Options (used by CORS preflight)
 curl -v -X OPTIONS http://localhost:8080/
 
-# Get all notes 
+# Get all Authors 
 curl -v -H "Accept: application/json" http://localhost:8080/ws4/rest/author 
 
-# Get single note
+# Get single Author
 curl -v -H "Accept: application/json" http://localhost:8080/ws4/rest/author/FF
 
-# Get primitive type (number of notes)
+# Get primitive type (number of authors)
 curl -v -H "Accept: application/json" http://localhost:8080/ws4/rest/author/count
 
 # Create new (NOTE: exception if run more times ... use delete) Using form parameters
-curl -v -X POST  http://localhost:8080/ws4/rest/author --data "id=QQ&firstName=Qbert&lastName=Qson&email=qson@mail"
+curl -v POST  http://localhost:8080/ws4/rest/author --data "id=QQ&firstName=Qbert&lastName=Qson&email=qson@mail"
 
 # Create new (NOTE: exception if run more times ... use delete) Using JSON
-curl -v -X POST -H "Content-Type: application/json"\
-http://localhost:8080/ws4/rest/author --data '{"id":"XX", "firstName": "Xbert", "lastName": "Xson", "email":"xson@mail, "address" : ""}'
+curl -v -H "Content-Type: application/json" POST http://localhost:8080/ws4/rest/author --data '{"id":"XX", "firstName": "Xbert", "lastName": "Xson", "email":"xson@mail", "address" : null}'
 
-# Update a note
-curl -v -X PUT http://localhost:8080/ws4/rest/author --data '{"id":"XX", "firstName": "NEWbert", "lastName": "NEWson", "email":"NEW@mail, "address" : ""}'
+# Update an Author
+curl -v PUT http://localhost:8080/ws4/rest/author/XX --data '{"id":"YY", "firstName": "NEWbert", "lastName": "NEWson", "email":"NEW@mail, "address" : null}'
 
 # Delete (last part of URL is id)
 curl -v -X DELETE http://localhost:8080/ws4/rest/author/XX 
 
-
+# Testing CORS use other origin 
+curl -v -H "Origin: http://www.example.com" -H "Accept: application/json" http://localhost:8080/ws4/rest/author
 
