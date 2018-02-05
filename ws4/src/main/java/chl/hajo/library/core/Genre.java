86686c8b1020084d6d8634cc41aece1761a01ffd
Betina Andersson <kungen@Betinas-MacBook-Pro.local>
2018-02-05 09:16:30 +0100

    ws 4 godkänd

---
+++ ws4/src/main/java/chl/hajo/library/core/Genre.java
@@ -0,0 +1,24 @@
+
+package chl.hajo.library.core;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * The genre of the book
+ * @author hajo
+ */
+public enum Genre {
+    THRILLER, NOVEL, ROMANTIC_NOVEL, BIOGRAPHY; 
+    
+    private static final List<Genre> VALUES =
+    Collections.unmodifiableList(Arrays.asList(values()));
+  private static final int SIZE = VALUES.size();
+  private static final Random RANDOM = new Random();
+
+  public static Genre randomLetter()  {
+    return VALUES.get(RANDOM.nextInt(SIZE));
+  }
+}
